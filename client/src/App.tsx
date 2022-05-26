b8aafb4f996a9bd296059c9f03e4ecfebfad12fe
Oliver Aarnikoivu <oliver.aarnikoivu@motion-s.com>
2022-05-26 15:14:22 +0200

    ...

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { Terminal } from "./components/Terminal/Terminal";
 import { useKeyPress } from "./hooks/useKeyPress";
 
 interface Input {
-  line: number;
   disabled: boolean;
   text: string;
 }
@@ -13,28 +12,20 @@ function App() {
   const arrowDownPressed = useKeyPress("ArrowDown");
   const enterPressed = useKeyPress("Enter");
   const [index, setIndex] = useState<number>(0);
-  const [input, setInput] = useState<Input[]>([
-    { line: 0, disabled: false, text: "" },
-  ]);
+  const [input, setInput] = useState<Input[]>([{ disabled: false, text: "" }]);
 
   useEffect(() => {
-    let prevLine: number = 0;
-
     if (enterPressed) {
       setInput((input: Input[]) =>
         input.map((item: Input, i: number) => {
           if (i === input.length - 1) {
-            prevLine = item.line;
             item.disabled = true;
           }
           return item;
         })
       );
-      setInput((old: Input[]) => [
-        ...old,
-        { line: prevLine + 1, disabled: false, text: "" },
-      ]);
-      setIndex((i: number) => i + 1);
+      setInput((old: Input[]) => [...old, { disabled: false, text: "" }]);
+      setIndex(input.length - 1 + 1);
     }
   }, [enterPressed]);
 
@@ -45,25 +36,25 @@ function App() {
   }, [arrowUpPressed]);
 
   useEffect(() => {
-    if (index < input.length - 1 && arrowDownPressed) {
+    if (arrowDownPressed) {
       setIndex((i) => i + 1);
     }
   }, [arrowDownPressed]);
 
   useEffect(() => {
-    try {
-      setInput((input: Input[]) =>
-        input.map((item: Input, i: number) => {
-          if (i === input.length - 1) {
+    setInput((input: Input[]) =>
+      input.map((item: Input, i: number) => {
+        if (i === input.length - 1) {
+          try {
             item.text = input[index].text;
+          } catch (err) {
+            item.text = "";
           }
-          return item;
-        })
-      );
-    } catch (err) {
-      console.log(err);
-    }
-  }, [index, input.length]);
+        }
+        return item;
+      })
+    );
+  }, [index]);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
@@ -74,10 +65,10 @@ function App() {
 
   return (
     <div style={{ marginLeft: "20px", marginTop: "20px" }}>
-      {input.map((item) => {
+      {input.map((item, i) => {
         return (
           <Terminal
-            key={item.line}
+            key={i}
             value={item.text}
             disabled={item.disabled}
             onChange={handleInputChange}
